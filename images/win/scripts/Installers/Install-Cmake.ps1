7750d0a80141de38207dab50bd11981010210e43
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:11 -0500

    Initialize releases/ubuntu18/20200428

---
+++ images/win/scripts/Installers/Install-Cmake.ps1
@@ -0,0 +1,6 @@
+################################################################################
+##  File:  Install-Cmake.ps1
+##  Desc:  Install Cmake
+################################################################################
+
+Choco-Install -PackageName cmake.install -ArgumentList "--installargs",'ADD_CMAKE_TO_PATH=""System""'
