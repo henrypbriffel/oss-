96dc3e0c91b941b6d53e68cf7bf7c17189e9d018
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:38 -0500

    Initialize releases/ubuntu16/20200517

---
+++ images/win/scripts/ImageHelpers/ImageHelpers.psm1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param()
+
+. $PSScriptRoot\PathHelpers.ps1
+. $PSScriptRoot\InstallHelpers.ps1
+. $PSScriptRoot\MarkdownHelpers.ps1
+. $PSScriptRoot\ChocoHelpers.ps1
+
+Export-ModuleMember -Function @(
+    'Test-MachinePath'
+    'Get-MachinePath'
+    'Set-MachinePath'
+    'Add-MachinePathItem'
+    'Get-SystemVariable'
+    'Set-SystemVariable'
+    'Install-Binary'
+    'Install-VisualStudio'
+    'Get-ToolcachePackages'
+    'Get-ToolsetContent'
+    'Get-ToolsByName'
+    'Add-ContentToMarkdown'
+    'Add-SoftwareDetailsToMarkdown'
+    'Stop-SvcWithErrHandling'
+    'Set-SvcWithErrHandling'
+    'Start-DownloadWithRetry'
+    'Install-VsixExtension'
+    'Get-VSExtensionVersion'
+    'Get-WinVersion'
+    'Test-IsWin19'
+    'Test-IsWin16'
+    'Choco-Install'
+    'Extract-7Zip'
+)
---
+++ images/win/scripts/Installers/Install-NSIS.ps1
@@ -0,0 +1,10 @@
+################################################################################
+##  File:  Install-NSIS.ps1
+##  Desc:  Install NSIS
+################################################################################
+
+Choco-Install -PackageName nsis
+
+$NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
+Add-MachinePathItem $NsisPath
+$env:Path = Get-MachinePath
