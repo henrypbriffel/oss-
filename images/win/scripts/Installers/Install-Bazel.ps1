3a666b13418fc58500aa2d92350071588a62000b
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:03 -0500

    Initialize releases/ubuntu16/20200426

---
+++ images/win/scripts/Installers/Install-Bazel.ps1
@@ -0,0 +1,8 @@
+################################################################################
+##  File:  Install-Bazel.ps1
+##  Desc:  Install Bazel and Bazelisk (A user-friendly launcher for Bazel)
+################################################################################
+
+Choco-Install -PackageName bazel
+
+npm install -g @bazel/bazelisk
---
+++ images/win/scripts/Installers/Install-KubernetesCli.ps1
@@ -0,0 +1,6 @@
+################################################################################
+##  File:  Install-KubernetesCli.ps1
+##  Desc:  Install KubernetesCli
+################################################################################
+
+Choco-Install -PackageName kubernetes-cli
