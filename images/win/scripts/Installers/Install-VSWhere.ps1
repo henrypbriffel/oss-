12e3e50adb05a454ce79734644f7fabbfa5463e3
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:06 -0500

    Initialize releases/ubuntu18/20200426

---
+++ images/win/scripts/Installers/Install-VSWhere.ps1
@@ -0,0 +1,6 @@
+################################################################################
+##  File:  Install-VSWhere.ps1
+##  Desc:  Install latest stable version of VSWhere
+################################################################################
+
+Choco-Install -PackageName vswhere
