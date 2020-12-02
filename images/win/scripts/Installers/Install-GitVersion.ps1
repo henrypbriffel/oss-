6c958de7776b5522562f084ee8d169943574ae4e
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:59:48 -0500

    Initialize releases/win16/20200621

---
+++ images/win/scripts/Installers/Install-GitVersion.ps1
@@ -0,0 +1,6 @@
+################################################################################
+##  File:  Install-GitVersion.ps1
+##  Desc:  Install GitVersion
+################################################################################
+
+Choco-Install -PackageName gitversion.portable
