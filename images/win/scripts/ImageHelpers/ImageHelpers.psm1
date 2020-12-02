04362483b581e4c408ab418b9307c8026defac51
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:16 -0500

    Initialize releases/ubuntu16/20200430

---
+++ images/win/scripts/ImageHelpers/ImageHelpers.psm1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param()
+
+. $PSScriptRoot\PathHelpers.ps1
+. $PSScriptRoot\InstallHelpers.ps1
+. $PSScriptRoot\MarkdownHelpers.ps1
+. $PSScriptRoot\ChocoHelpers.ps1
+
+Export-ModuleMember -Function @(
+    'Test-MachinePath'
+    'Get-MachinePath'
+    'Set-MachinePath'
+    'Add-MachinePathItem'
+    'Get-SystemVariable'
+    'Set-SystemVariable'
+    'Install-Binary'
+    'Get-ToolcachePackages'
+    'Get-ToolsByName'
+    'Add-ContentToMarkdown'
+    'Add-SoftwareDetailsToMarkdown'
+    'Stop-SvcWithErrHandling'
+    'Set-SvcWithErrHandling'
+    'Start-DownloadWithRetry'
+    'Install-VsixExtension'
+    'Get-VSExtensionVersion'
+    'Get-WinVersion'
+    'Test-IsWin19'
+    'Test-IsWin16'
+    'Choco-Install'
+)
