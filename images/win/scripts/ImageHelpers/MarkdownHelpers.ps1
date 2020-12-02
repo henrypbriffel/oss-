1b8eda8c993a3dee9b64845ff7a0a9f5556596ec
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 18:58:13 -0500

    Initialize releases/ubuntu18/20200430

---
+++ images/win/scripts/ImageHelpers/MarkdownHelpers.ps1
@@ -0,0 +1,25 @@
+function Add-ContentToMarkdown {
+    
+    param(
+        $Content = ""
+    )
+
+    Add-Content 'C:\InstalledSoftware.md' $Content
+}
+
+
+function Add-SoftwareDetailsToMarkdown {
+    [CmdletBinding()]
+    param(
+        $SoftwareName = "",
+        $DescriptionMarkdown = ""
+    )
+
+$Content = @"
+
+## $SoftwareName
+
+$DescriptionMarkdown
+"@
+   Add-ContentToMarkdown -Content $Content
+}
