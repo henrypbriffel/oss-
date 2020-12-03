1d29a3a1a6f9c98fec4e3576770932973c06d15a
Henry P Briffel <61019780+henrypbriffel@users.noreply.github.com>
2020-12-02 19:00:46 -0500

    Initialize releases/macos-10.15/20200716

---
+++ images/win/scripts/Installers/Install-Sbt.ps1
@@ -0,0 +1,18 @@
+################################################################################
+##  File:  Install-Sbt.ps1
+##  Desc:  Install sbt for Windows
+################################################################################
+$ErrorActionPreference = "Stop"
+
+Import-Module -Name ImageHelpers
+
+# Install the latest version of sbt.
+# See https://chocolatey.org/packages/sbt
+Choco-Install -PackageName sbt
+
+$env:SBT_HOME="${env:ProgramFiles(x86)}\sbt"
+
+# Add sbt binaries to the path
+Add-MachinePathItem "$env:SBT_HOME\bin"
+
+Invoke-PesterTests -TestFile "Tools" -TestName "Sbt"
